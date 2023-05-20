b3ac34f52e2125c1438d59e565dfa0b858566d63
masecamo <134107701+masecamo@users.noreply.github.com>
2023-05-20 11:58:34 -0400

    prmer
    informacion 1

---
+++ cadena.py
@@ -0,0 +1,38 @@
+C1 = "BNA ES MUY GRANDE"
+print(C1[1])
+
+
+
+
+
+x =C1.replace(' ',  '-')
+
+
+print(x)
+
+
+
+
+C2 = ""
+
+for i in C1:
+     if i != ' ':
+       C2 = C2+i
+     else:
+         C2 = C2+ '-'
+print(C2)         
+          
+
+
+       
+
+
+
+print (C1.index('A'))
+
+for i in C1:
+      if i == 'A':
+           print(i)
+
+
+        
